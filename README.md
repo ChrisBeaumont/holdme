5c3da95d7be381ad48a2d3dd6b5d5117c2dbe123
Chris Beaumont <beaumont@hawaii.edu>
2015-02-16 08:27:58 -0800

    Documentation

---
+++ README.md
@@ -0,0 +1,25 @@
+# Holdme
+
+Holdme is a fast Texas Hold'em Poker odds library written in Cython.
+
+### Usage
+
+```python
+from holdme import Card, Hand, headsup
+
+h1 = Hand('AC AD')
+h2 = Hand('KS TH')
+
+pwin, plose = headsup(h1, h2)
+print pwin, plose  # 0.853392855474 0.143615269251
+
+flop = Hand('KS KH TD')
+pwin, plose = headsup(h1, h2, flop)
+print pwin, plose # 0.918181818182 0.0818181818182
+
+community = flop + Card('AH') + Card('AS')
+pwin, plose = headsup(h1, h2, community)
+print pwin, plose # 1, 0
+
+print Hand('2C 3C 4C 5C 6C').name  # Straight Flush (6 high)
+```
--- holdme/core.py
+++ holdme/core.py
@@ -54,13 +54,17 @@ class Card(object):
 class Hand(object):
 
     def __init__(self, name=''):
-        self._cards = [Card(n) for n in name.split()]
+        self._cards = name
+        if isinstance(self._cards, str):
+            self._cards = [Card(n) for n in name.split()]
 
     @property
     def rank(self):
         if len(self._cards) == 5:
             return _lib.score5(*(c.bitmask for c in self._cards))
-        return _lib.score7(*(c.bitmask for c in self._cards))
+        elif len(self._cards) == 7:
+            return _lib.score7(*(c.bitmask for c in self._cards))
+        raise ValueError("Can only compute hand strength for 5 or 7 card hands")
 
     @property
     def name(self):
@@ -73,6 +77,19 @@ class Hand(object):
     def __len__(self):
         return len(self._cards)
 
+    def __str__(self):
+        return "Hand('%s')" % (' '.join(str(c) for c in self._cards))
+
+    def __add__(self, other):
+        if isinstance(other, Hand):
+            return Hand(self.cards + other.cards)
+        elif isinstance(other, Card):
+            return Hand(self.cards + [other])
+        else:
+            raise TypeError("Can only add Hand or Card to Hand")
+
+    __repr__ = __str__
+
 
 def _mask2rank(mask):
     result = []
@@ -84,11 +101,16 @@ def _mask2rank(mask):
 
 
 def deck():
+    """
+    Return a list of all 52 Card instances
+    """
     return [Card.from_index(i) for i in range(52)]
 
 
 def hand_name(score):
-
+    """
+    Convert holdme's internal hand rank to a human-readable name
+    """
     tid = score >> 26
     b1 = _mask2rank((score >> 13) & ((1 << 13) - 1))
     b2 = _mask2rank(score & ((1 << 13) - 1))
@@ -114,6 +136,24 @@ def hand_name(score):
 
 
 def headsup(h1, h2, community=None):
+    """
+    Compute the probability that Hand 1 beats/loses to Hand 2 by
+    enumerating over all holdem games
+
+    Parameters
+    ----------
+    h1 : Hand
+       The first hand
+    h2 : Hand
+       The second hand
+    community : Hand (optional)
+       Any previously dealt community cards
+
+    Returns
+    -------
+    pwin, plose : (float, float)
+       The probability that h1 beats/loses to h2
+    """
     community = community or Hand()
     args = [c.bitmask for h in [h1, h2, community] for c in h._cards]
     if len(community) == 0:
@@ -122,5 +162,9 @@ def headsup(h1, h2, community=None):
         result = _lib.enumerate_headsup_flop(*args)
     elif len(community) == 4:
         result = _lib.enumerate_headsup_turn(*args)
+    else:
+        s1 = _lib.score7(*[c.bitmask for h in [h1, community] for c in h._cards])
+        s2 = _lib.score7(*[c.bitmask for h in [h2, community] for c in h._cards])
+        return float(s1 > s2), float(s1 < s2)
 
     return result['pwin'], result['plose']
